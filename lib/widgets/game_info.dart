3b0b290d1f7c92c1cf484e6e0d5cd085886a507e
hokieknight <dale.mrotek@gmail.com>
2024-03-09 21:59:36 -0500

    fix game info screen layout

--- lib/widgets/game_info.dart
+++ lib/widgets/game_info.dart
@@ -10,39 +10,65 @@ class GameInfoWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final players = gameState.players;
 
-    return Expanded(
-      child: Column(
+    return Container(
+      margin: const EdgeInsets.all(4),
+      padding: const EdgeInsets.all(8),
+      //decoration: BoxDecoration(border: Border.all(color: Colors.red)),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(
-            children: [
-              //const Text('Game Type: '),
-              Text(gameState.gameType),
-            ],
+          Container(
+            margin: const EdgeInsets.all(4),
+            padding: const EdgeInsets.all(8),
+            //decoration: BoxDecoration(border: Border.all(color: Colors.red)),
+            child: Image.asset(
+              'images/CoverSecretSith.jpg',
+              height: 250,
+              //width: 100,
+              fit: BoxFit.fitHeight,
+            ),
           ),
-          //const SizedBox(width: 10),
-          Row(
-            children: [
-              const Text('# Players: '),
-              Text('${players.length} / ${gameState.numPlayers}'),
-            ],
-          ),
-          Row(
-            children: [
-              const Text('Game ID: '),
-              SelectableText(gameState.id),
-            ],
-          ),
-          Expanded(
-            child: ListView.builder(
-              padding: const EdgeInsets.all(8),
-              itemCount: players.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Text('Player: ${players[index]['name']}');
-              },
+          Container(
+            margin: const EdgeInsets.all(4),
+            padding: const EdgeInsets.all(8),
+            //decoration: BoxDecoration(border: Border.all(color: Colors.red)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    const Text('Game ID: '), //aBmTuXAioWHuVKK1ymlk
+                    SelectableText(gameState.id),
+                  ],
+                ),
+                Text('Name: ${gameState.name}'),
+                Text('#Players: ${players.length} / ${gameState.numPlayers}'),
+                Container(
+                  margin: const EdgeInsets.all(4),
+                  padding: const EdgeInsets.all(8),
+                  //decoration:
+                  //    BoxDecoration(border: Border.all(color: Colors.red)),
+                  width: 200,
+                  height: 200,
+                  child: ListView.builder(
+                    //padding: const EdgeInsets.all(8),
+                    itemCount: players.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Text(
+                          'Player ${index + 1}: ${players[index]['name']}');
+                    },
+                  ),
+                ),
+              ],
             ),
           ),
         ],
       ),
     );
+
+    // Column(
+    //   children: [
+    //   ],
+    // );
   }
 }
